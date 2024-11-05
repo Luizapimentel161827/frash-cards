0333561a7ff054cd6e9762b1ca4f1b07672a9ffa
Anira <00001109028994sp@al.educacao.sp.gov.br>
2024-11-05 15:05:43 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Geografia',
+    'Qual é a capital de Tocantins?',
+    'Palmas.'
+)
+
+criaCartao(
+    'Biologia',
+    'o que é mitose?',
+    'é um tipo de divisão celular em que uma célula (a célula-mãe) se divide para produzir duas novas células (as filhas) que são geneticamente idênticas a ela.'
+)
+
+criaCartao(
+    'português',
+    'O que é adjetivo?',
+    'é uma classe de palavras que atribui características aos substantivos, ou seja, ele indica suas qualidades e estados.'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz oi em Inglês?',
+    'Oi em ingles é HI (RAI)'
+)
